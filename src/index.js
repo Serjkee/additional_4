7f582e9b39a45d03737a5d6b076b9ad58e26a7c8
Serj <dreamstar.pwr@gmail.com>
2017-09-24 13:06:40 +0300

    multiply 2 numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-module.exports = function multiply(first, second) {
-  // your solution
-}
+module.exports = function multiply(s1, s2) {
+	if( s1.length < s2.length )
+	{
+		var tmp = s2;
+		s2 = s1;
+		s1 = tmp;
+	}
+	var a1 = s1.split( '' );
+	var a2 = s2.split( '' );
+	var r = [];
+	var rlen = a1.length + a2.length; 
+	for( var k = 0; k < rlen; k++ )
+		r[ k ] = 0;
+	for( var j = 0; j < a2.length; j++ ) 
+		for( var i = 0; i < a1.length; i++ )
+			r[ 1 + i + j ] += a1[ i ] * a2[ j ];   
+		
+		for( var k = rlen - 1; k > 0; k-- )
+			if( r[ k ] >= 10 )
+			{
+				r[ k - 1 ] += Math.floor( r[ k ] / 10 );
+				r[ k ] %= 10;
+			}
+			if( r[ 0 ] == 0 )
+				delete r[ 0 ];
+			return r.join( '' );
+		}
